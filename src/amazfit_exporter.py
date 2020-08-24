e43e526798b711da04a9dcc0e93c239348241017
Christoph Etzel <christoph.etzel@informatik.uni-augsburg.de>
2020-08-24 16:18:46 +0200

    Add logger and simplify last update time calculation

--- src/amazfit_exporter.py
+++ src/amazfit_exporter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import logging
 import sqlite3
 import pathlib
 from datetime import datetime
@@ -10,13 +11,15 @@ from amazfit_exporter_gpx import db_to_gpx
 # global variable for the database cursor
 cursor = None
 
+logger = logging.getLogger(__name__)
+
 # FIXME remove or do
 def local_date_to_utc(date):
     return datetime.utcfromtimestamp(int(date / 1000))
 
 def get_activities_data(begin_time):
     # Use begin_time to load only data beyond that time
-    cursor.execute('SELECT track_id, start_time, end_time, calorie, type, content FROM sport_summary WHERE track_id >=' + str(begin_time) + ' AND (type BETWEEN 1 AND 5)')
+    cursor.execute('SELECT track_id, start_time, end_time, calorie, type, content FROM sport_summary WHERE track_id >=' + str(begin_time) + ' AND (type BETWEEN 1 AND 15)')
     return cursor.fetchall()
 
 def get_track_data(begin_time):
@@ -35,6 +38,8 @@ def document_to_string(document):
     return etree.tostring(document.getroot(), xml_declaration=True, encoding="UTF-8", pretty_print=True)
 
 def start_export(db,dest,begin_time):
+    logger.info("Started export")
+
     new_update_begin_time = -1
 
     # Connect to the sport database
@@ -53,14 +58,11 @@ def start_export(db,dest,begin_time):
             amazfit_exporter_config.heart_rate_data = get_heart_rate_data(begin_time)
             db_to_tcx(dest)
             db_to_gpx(dest)
-            # identifier = amazfit_exporter_config.activities[,]
-            identifier = 0
-            for activity in amazfit_exporter_config.activities:
-                if identifier < activity['track_id']:
-                    identifier = activity['track_id']
-            if new_update_begin_time < identifier:
-                new_update_begin_time = identifier 
+            # search for highest track_id as new update begin time
+            new_update_begin_time = max([act[0] for act in amazfit_exporter_config.activities], default=-1) 
+            logger.info("Highest track id found %d", new_update_begin_time)
     except sqlite3.OperationalError:
-        print("Error: Database not readable!")
+        logger.error("Error: Database not readable! Check database: '%s'", db)
     
+    logger.info("Finished export")
     return new_update_begin_time
