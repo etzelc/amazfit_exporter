760d9f4d5327193decbfd2aafddb7d7458830317
Christoph Etzel <christoph.etzel@informatik.uni-augsburg.de>
2020-08-24 16:17:54 +0200

    Add logger configuration and global sport mapping dictionary

--- src/amazfit_exporter_config.py
+++ src/amazfit_exporter_config.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+import logging, sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 activities = []
 trackpoint_data = []
 heart_rate_data = []
-x = 1
+
+# Map Amazfit DB to strings
+SPORT_MAPPING = {
+    None: "Other",
+    1: "Running", # Running
+    2: "Running", # Walking
+    3: "Running", # Trail Running
+    4: "Running", # Indoor Running
+    5: "Biking", # Biking
+    6: "Other", #Walking
+    7: "Running", # Trail Runing
+    8: "Running", # Treadmil
+    9: "Biking", # Outdoor Biking
+    10: "Biking", # Indoor Biking
+}
